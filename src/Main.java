c820f7cb40010a0108479338cc23ac91dadaaa76
thetharz <tharinduimalka915@gmail.com>
2023-07-13 18:57:56 +0530

    concatenate

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        char[] ch = {'J','a','v','a',' ','S','t','r','i','n','g'};
+        /*char[] ch = {'J','a','v','a',' ','S','t','r','i','n','g'};
         String s = new String(ch);
         System.out.println(ch);
 
         String str = "Java String";
-        System.out.println(str);
+        System.out.println(str);*/
+
+        //the + can use to concatenate strings
+        String p = "Hello";
+        String q = "World";
+        System.out.println(p+q); // concatenate the p and q
+
+        String a = "Hello";
+        System.out.println("Hello".equals(a));//check whether strings are equal or not here they are equal
+        System.out.println("hello".equals(a));
+        System.out.println("hello".equalsIgnoreCase(a));
     }
 }
