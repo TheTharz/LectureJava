85409270e42ef60d816dec0d473e2b31b5c0be83
thetharz <tharinduimalka915@gmail.com>
2023-07-13 18:44:33 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        char[] ch = {'J','a','v','a',' ','S','t','r','i','n','g'};
+        String s = new String(ch);
+        System.out.println(ch);
+
+        String str = "Java String";
+        System.out.println(str);
+    }
+}
