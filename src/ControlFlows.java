a6badf39961d37bcd76f906443728a23d8acd24d
thetharz <tharinduimalka915@gmail.com>
2023-08-03 19:12:34 +0530

    bubble

--- src/ControlFlows.java
+++ src/ControlFlows.java
@@ -1,6 +1,6 @@
 public class ControlFlows {
     public static void main(String[] args){
-        int marks = Integer.parseInt(args[0]);
+        /*int marks = Integer.parseInt(args[0]);
 
         if(marks>70){
             System.out.println("Grade A");
@@ -10,6 +10,23 @@ public class ControlFlows {
             System.out.println("Grade C");
         }else{
             System.out.println("Grade D");
+        }*/
+
+        int arr[] = {3,2,6,7,8};
+        int n = arr.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for(int k=0;k<n;k++){
+            System.out.println(arr[k]);
         }
     }
 }
